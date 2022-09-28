e9afbc896473105dc335beb172b0112b8140d2d4
Dave Boland <daveboland@Daves-MacBook-Pro.local>
2022-09-28 12:34:17 +0100

    Added button click test

--- src/components/DrawCardButton/DrawCardButton.test.js
+++ src/components/DrawCardButton/DrawCardButton.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent} from "@testing-library/react";
 import "@testing-library/jest-dom";
 import { DrawCardButton } from "./DrawCardButton";
 
@@ -7,7 +7,21 @@ describe("draw card tests", () => {
   it("renders draw card button", () => {
     expect.assertions(1);
     render(<DrawCardButton />);
-    const container = screen.getByText("Draw card");
-    expect(container).toBeInTheDocument();
+    const button = screen.getByText("Draw card");
+    expect(button).toBeInTheDocument();
   });
+
+  it("draw button handles a click correctly", () => {
+    let clicked = false;
+    const drawCard = (event) => {
+      clicked = true;
+    };
+    const { getByText } = render(<DrawCardButton drawCard={drawCard} />);
+    const button = getByText("Draw card");
+  
+    expect(button).toBeTruthy();
+    fireEvent.click(button);
+    expect(clicked).toBeTruthy();
+  });
+
 });
